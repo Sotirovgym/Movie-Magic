4fab36e3925ed1cfc6336be1a2c944afaa2bf6e2
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-19 11:10:54 +0200

    Adding cat model

---
+++ src/models/cat.js
@@ -0,0 +1,14 @@
+class Cat{
+    constructor(id, title, genre, director, year, imageURL, rating, description){
+        this.id = id;
+        this.tit = title;
+        this.genre = genre;
+        this.director = director;
+        this.year = year;
+        this.imageURL= imageURL;
+        this.rating = rating;
+        this.description = description;
+    }
+}
+
+module.exports = Cat;
