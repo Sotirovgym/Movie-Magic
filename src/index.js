b42fc92776091eb3da46a7adc926aaf1402a239d
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-19 10:52:09 +0200

    Adding routes module and render the home page.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
+const routes = require('./routes');
 
 const app = express();
 const port = 4200;
 
 configExpress(app);
 configHandlebars(app);
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`The app is running ${port}...`);
