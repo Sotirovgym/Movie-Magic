b154a027206c929394a3a30be6f519dd2223602b
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 11:26:34 +0200

    Setup DB connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
 const routes = require('./routes');
@@ -11,6 +13,11 @@ configHandlebars(app);
 
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`The app is running ${port}...`);
-});
+mongoose.connect('mongodb://localhost:27017/magic-movies')
+    .then(() => {
+        console.log('DB Connected');
+
+        // We start the server when the DB is connected
+        app.listen(port, () => console.log(`The app is running ${port}...`));
+    })
+    .catch(err => console.log(err));
