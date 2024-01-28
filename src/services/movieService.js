98f916de7edd9e982799153cd7b98eda0edc1f25
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 12:01:28 +0200

    Removing async and await on the service level

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const Movie = require('../models/Movie');
 
-exports.create = async (movieData) => {
-    return await Movie.create(movieData);
+exports.create = (movieData) => {
+    return Movie.create(movieData);
 }
 
-exports.getAll = async () => {
-    // movies is array of Document which is not an object, that's why we use lean() on the query result to get the object
-    const movies = await Movie.find().lean();
-    return movies;
+exports.getAll = () => {
+    // Movie.find() returns an array of Documents, that's why we use lean() on the query result to get the object
+    return Movie.find();
 }
 
 exports.getByID = (id) => {
