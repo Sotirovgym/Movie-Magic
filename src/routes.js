77949d76bcaa1f506c3debfe4b2b2b7543b4b4a8
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 16:38:06 +0200

    Include castController to the routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const searchController = require('./controllers/searchController');
+const castController = require('./controllers/castController');
 
 router.use(homeController);
 router.use(movieController);
 router.use(searchController);
+router.use(castController);
 
 router.get('*', (req, res) => {
     res.render('404');
