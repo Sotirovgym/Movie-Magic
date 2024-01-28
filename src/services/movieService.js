6141711852a1e9131386e3966bcffbd8ea4d0939
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 11:45:48 +0200

    Load movies from the DB

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const data = require('../config/database.json');
 const Movie = require('../models/Movie');
 
 exports.create = async (movieData) => {
     return await Movie.create(movieData);
 }
 
-exports.getAll = () => {
-    return data;
+exports.getAll = async () => {
+    // movies is array of Document which is not an object, that's why we use lean() on the query result to get the object
+    const movies = await Movie.find().lean();
+    return movies;
 }
 
 exports.getByID = (id) => {
