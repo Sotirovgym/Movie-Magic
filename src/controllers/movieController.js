db4c200997712437f7d5d643984aef98f01418a8
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 16:45:41 +0200

    Fix the routes in movieController

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const movieService = require('../services/movieService');
 
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('movie/create'); 
 });
 
 router.post('/create', async (req, res) => {
@@ -18,7 +18,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/details/:id', async (req, res) => {
     const movie = await movieService.getByID(req.params.id).lean();
-    res.render('details', movie);
+    res.render('movie/details', movie);
 });
 
 module.exports = router;
