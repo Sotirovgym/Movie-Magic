90ed31df868f6193769f1d733daa3cbff540075a
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-28 11:28:04 +0200

    Create a mongoose movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,14 +1,42 @@
-class Movie{
-    constructor(id, title, genre, director, year, imageURL, rating, description){
-        this.id = id;
-        this.tit = title;
-        this.genre = genre;
-        this.director = director;
-        this.year = year;
-        this.imageURL= imageURL;
-        this.rating = rating;
-        this.description = description;
-    }
-}
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        required: true
+    },
+    director:{
+        type: String,
+        required: true
+    },
+    year:{
+        type: Number,
+        required: true,
+        min: 1900,
+        max: 2030
+    },
+    rating:{
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    description:{
+        type: String,
+        required: true,
+        maxLength: 2000
+    },
+    imageURL:{
+        type: String,
+        required: true,
+        match: /^https?/ // regex validation
+    },
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
 
 module.exports = Movie;
