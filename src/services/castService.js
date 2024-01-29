196616a34af1ffe6a7fb3503f3def76b1bb0856f
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-29 17:51:56 +0200

    Add functions for getAll and getManyByIDs in the castService

--- src/services/castService.js
+++ src/services/castService.js
@@ -3,3 +3,11 @@ const Cast = require('../models/Cast');
 exports.create = (data) => {
     return Cast.create(data);
 }
+
+exports.getAll = () => {
+    return Cast.find();
+}
+
+exports.getManyByIDs = (castIDs) => {
+    return Cast.find({ _id: { $in: castIDs }});
+}
