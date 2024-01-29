cfdc41da0c1233bb8f16716f7642c6f414886fdb
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-29 10:51:29 +0200

    Implementing create cast

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const castService = require('../services/castService');
 
 router.get('/cast/create', (req, res) => {
     res.render('cast/create');
 });
 
-router.post('/cast/create', (req, res) => {
-    
+router.post('/cast/create', async (req, res) => {
+    await castService.create(req.body);
+    res.redirect('/');
 });
 
 module.exports = router;
