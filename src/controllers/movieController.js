2dde1f9f706b59aff1c3fdb707378d31c88e63e7
Georgi Sotirov <sotirovgym@abv.bg>
2024-01-29 17:54:51 +0200

    Load casts dynamically in the movie details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const movieService = require('../services/movieService');
+const castService = require('../services/castService');
 
 router.get('/create', (req, res) => {
     res.render('movie/create'); 
@@ -16,9 +17,29 @@ router.post('/create', async (req, res) => {
     }
 });
 
+router.get('/movies/:id/attach', async (req, res) => {
+    const movie = await movieService.getByID(req.params.id).lean();
+    const casts = await castService.getAll().lean();
+    // TODO: remove already added casts
+    res.render('movie/attach', { movie, casts });
+});
+
+router.post('/movies/:id/attach', async (req, res) => {
+    const movieID = req.params.id;
+    await movieService.attachCast(movieID, req.body.cast);
+    res.redirect(`/details/${movieID}`);
+});
+
+// Just an example of how to get the casts without using populate
+// router.get('/details/:id', async (req, res) => {
+//     const movie = await movieService.getByID(req.params.id).lean();
+//     const casts = await castService.getManyByIDs(movie.casts).lean();
+//     res.render('movie/details', { movie, casts });
+// });
+
 router.get('/details/:id', async (req, res) => {
     const movie = await movieService.getByID(req.params.id).lean();
-    res.render('movie/details', movie);
+    res.render('movie/details', { movie });
 });
 
 module.exports = router;
