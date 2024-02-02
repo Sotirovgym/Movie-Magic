aa266881801c21160b425d58f28af73139a5c55f
Georgi Sotirov <sotirovgym@abv.bg>
2024-02-02 11:03:39 +0200

    Add userController

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const searchController = require('./controllers/searchController');
 const castController = require('./controllers/castController');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(movieController);
 router.use(searchController);
 router.use(castController);
+router.use(userController);
 
 router.get('*', (req, res) => {
     res.render('404');
