bd63bf39768672d3159ff3635e4aae8734b330e1
Georgi Sotirov <sotirovgym@abv.bg>
2024-02-04 18:00:00 +0200

    Create User model

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', async function() {
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.virtual('rePassword')
+    .set(function(value){
+        if (value !== this.password){
+            throw new mongoose.MongooseError('Password missmatch!');
+        }
+    });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
