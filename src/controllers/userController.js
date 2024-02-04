5f365156d95b264265a21a14edf644e2e22ec6eb
Georgi Sotirov <sotirovgym@abv.bg>
2024-02-04 18:00:28 +0200

    Add register and login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 
+const userService = require('../services/userService');
+
 router.get('/register', (req, res) => {
     res.render('user/register');
-})
+});
+
+router.post('/register', async (req, res) => {
+    await userService.register(req.body);
+    res.redirect('/login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('user/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const token = await userService.login(email, password);
+    res.cookie('auth', token);
+    res.redirect('/');
+});
 
 module.exports = router;
